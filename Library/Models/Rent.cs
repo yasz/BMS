94fa600b8f3fba1c492f121379afe47e5d62e904
peter.yang <yangjiahao@outlook.com>
2019-10-05 08:32:09 +0800

    表单验证功能测试

--- Library/Models/Rent.cs
+++ Library/Models/Rent.cs
@@ -10,7 +10,14 @@ namespace Library.Models
     {
       
         public Guid RentId { get; set; }
+        [Required]
+        
+        [StringLength(20, ErrorMessage = "书籍编号必须为10-20位数字", MinimumLength = 10)]
         public string BookNo { get; set; }
+
+        [Required]
+//        [StringLength(6, ErrorMessage = "学号必须为6位数字")]
+        [RegularExpression("^.{8}|.{6}", ErrorMessage = "学号必须为6位或8位数字")]
         public string Vano { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
