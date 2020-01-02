159da17605aa0b0b8ab88980d95771f4ad0a31c1
peter.yang <yangjiahao@outlook.com>
2020-01-02 17:07:55 +0800

    删除about，和禁用缩放。

--- Library/wwwroot/ch1.js
+++ Library/wwwroot/ch1.js
@@ -11,3 +11,18 @@ window.MyPop = (msgs) => {
 window.MyAwait = () => {
     console.log("await...");
 }
+var scrollFunc = function (e) {
+    e = e || window.event;
+    if (e.wheelDelta && event.ctrlKey) {//IE/Opera/Chrome
+        event.returnValue = false;
+    } else if (e.detail) {//Firefox
+        event.returnValue = false;
+    }
+}
+
+
+/*注册事件*/
+if (document.addEventListener) {
+    document.addEventListener('DOMMouseScroll', scrollFunc, false);
+}//W3C
+window.onmousewheel = document.onmousewheel = scrollFunc;
